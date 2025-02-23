765f759646189d2cb36198cefb87dcce4a10beb3
EricW21 <ew21hi@gmail.com>
2025-02-23 13:22:23 +0000

    message dao

--- src/main/java/Service/MessageService.java
+++ src/main/java/Service/MessageService.java
@@ -1,5 +1,38 @@
 package Service;
+import Util.ConnectionUtil;
+import Model.Message;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import DAO.MessageDao;
 public class MessageService {
-
+    MessageDao MessageDao;
+    public MessageService() {
+        this.MessageDao = new MessageDao();
+    }
+    public MessageService(MessageDao Message) {
+        this.MessageDao= Message;
+    }
+    public List<Message> getAllMessages(){
+        return this.MessageDao.getAllMessages();
+    }
+    public Message createMessage(Message Message) {
+        return this.MessageDao.createMessage(Message);
+    }
+    public Message retrieveMessage(int id) {
+        return this.retrieveMessage(id);
+    }
+    public Message deleteMessage(int id) {
+        return this.deleteMessage(id);
+    }
+    public Message updateMessageText(int id, String message_text) {
+        return this.updateMessageText(id, message_text);
+    }
+    public List<Message> retrieveMessagesByUser(int user_id){
+        return this.retrieveMessagesByUser(user_id);
+    }
 }
