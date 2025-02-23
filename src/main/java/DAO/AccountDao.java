4c0310d5a2eaf2049914f7d65044a160c305c5fd
EricW21 <ew21hi@gmail.com>
2025-02-23 06:45:17 +0000

    account service

--- src/main/java/DAO/AccountDao.java
+++ src/main/java/DAO/AccountDao.java
@@ -6,8 +6,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+// import java.util.ArrayList;
+// import java.util.List;
 public class AccountDao {
     public Account newUser(Account account) {
         Connection connection = ConnectionUtil.getConnection();
--- src/main/java/Service/AccountService.java
+++ src/main/java/Service/AccountService.java
@@ -1,5 +1,27 @@
 package Service;
+import Util.ConnectionUtil;
+import Model.Account;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import DAO.AccountDao;
 public class AccountService {
+    private AccountDao accountDao;
+    public AccountService() {
+        this.accountDao = new AccountDao();
+    }
+    public AccountService(AccountDao account) {
+        this.accountDao= account;
+    }
+    public Account newUser(Account account) {
+        return this.accountDao.newUser(account);
+    }
+    public Account processLogin(Account account) {
+        return this.accountDao.processLogin(account);
+    }
 
 }
